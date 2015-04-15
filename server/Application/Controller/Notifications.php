f2fc621ace70f65ff5d30705e740cb274de89768
Vamsi Kollipara <vamsi.kollipara@divami.com>
2015-04-15 20:24:27 +0530

    Notifications re defined

--- server/Application/Controller/Notifications.php
+++ server/Application/Controller/Notifications.php
@@ -1,13 +1,22 @@
 <?php
-    Class Notifications {
-    	public function getNotifications($interpreter){
-    		$data = $interpreter->getData()->data;
+	Class Notifications {
+		public function getNotifications($interpreter){
+			$data = $interpreter->getData()->data;
 			$userId = $data->userId;
+			$limit = $data->limit;
 			
 			$db = Master::getDBConnectionMAnager();
-			$queryDetails = getQuery('getNotifications',array(id => $userId));
+			$queryDetails = getQuery('getNotifications',array(id => $userId, '@limit'=>$limit));
 			$resultObj = $db->multiObjectQuery($queryDetails);
+			$resultLen = count($resultObj);
+			for($i = 0; $i < $resultLen ; $i++ )  {
+				if($resultObj[$i]->type == 'M') {
+					Master::getLogManager()->log(DEBUG, MOD_MAIN, 'inside meeting invite');
+					$queryDetails = getQuery('getMeetingNotificationDetails',array(id => $resultObj[$i]->refId));
+					$resultObj[$i]->meetingDetails = $db->singleObjectQuery($queryDetails);
+				}
+			}
 			return $resultObj;
-    	}
-    }
+		}
+	}
 ?>
