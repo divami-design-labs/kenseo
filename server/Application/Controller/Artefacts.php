2919bd166cb48ef439a97542ebded5e745ef5a60
Vamsi Kollipara <vamsi.kollipara@divami.com>
2015-04-30 12:13:35 +0530

    limit implemented for share artefacts

--- server/Application/Controller/Artefacts.php
+++ server/Application/Controller/Artefacts.php
@@ -56,13 +56,14 @@
 			return $resultObj;
 		}
 		function getSharedArtefacts($interpreter) {
-			$data = $interpreter->getData();
-			$userid = $interpreter->getUser()->user_id;;
+			$data = $interpreter->getData()->data;
+			$userid = $interpreter->getUser()->user_id;
+			$limit = $data->limit;
 			
 			Master::getLogManager()->log(DEBUG, MOD_MAIN, $userid);
 			
 			$db = Master::getDBConnectionManager();
-			$querDetails = getQuery('getReviewRequests',array('userid'=>$userid));
+			$querDetails = getQuery('getReviewRequests',array('userid'=>$userid, '@limit' => $limit ));
 			$resultObj = $db->multiObjectQuery($querDetails);
 			return $resultObj;
 		}
