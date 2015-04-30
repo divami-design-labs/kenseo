387d9cd429d163eb19cfebb2b2f32f0a6d44f18e
Vamsi Kollipara <vamsi.kollipara@divami.com>
2015-04-30 13:00:12 +0530

    limiting people to max possible if no limit is mentioned in the project page

--- server/Application/Controller/People.php
+++ server/Application/Controller/People.php
@@ -3,7 +3,12 @@
     	public function getProjectsPeople($interpreter) {
     		$data = $interpreter->getData()->data;
 			$userId = $interpreter->getUser()->user_id;
-			$limit  =  $data->limit;
+			
+			if($data->limit) {
+				$limit  =  $data->limit;
+			} else {
+				$limit = 2147483647;
+			}
 			$db = Master::getDBConnectionManager();
 			$querDetails = getQuery('getPeopleInProjects',array('userid'=>$userId, '@limit' => $limit));
 			$resultObj = $db->multiObjectQuery($querDetails);
