eda5db54b4096d873ec2ee8bc578932c835c9489
Venkateshwar Kamlekar <venkateshwar@divami.com>
2015-05-21 10:59:11 +0530

    Added live server config and googleauth

--- server/config/config.php
+++ server/config/config.php
@@ -3,13 +3,12 @@
 global $AppGlobal;
 
 $AppGlobal['log'] = array();
-$AppGlobal['log']['path'] = "D://tmp/kenseo.log" ;
+$AppGlobal['log']['path'] = "/var/tmp/kenseo.log" ;
 $AppGlobal['log']['mode'] = "ON" ;
 $AppGlobal['log']['modules'] = "ALL" ;
+$AppGlobal['global']['domain'] = "http://kenseo.divami.com/";
 
-$AppGlobal['global']['domain'] = "http://localhost/kenseo/";
-$AppGlobal['store']['path'] = "D://tmp/";
-$AppGlobal['gloabl']['storeLocation'] = "D://tmp/";
+$AppGlobal['gloabl']['storeLocation'] = "/var/tmp/";
 
 $AppGlobal['cache'] = array();
 $AppGlobal['cache']['type'] = "apc";
