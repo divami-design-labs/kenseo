df1bf7783b96ed44b3a93fd1587695df013ccd37
Mr_Green <venkateshwar@divami.com>
2015-11-27 14:09:31 +0530

    gulp file modified

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -1,21 +1,25 @@
 var gulp = require('gulp');
 var fs = require('fs');
 var tap = require('gulp-tap');
+var sourcemaps = require('gulp-sourcemaps');
+// var template = require('gulp-lodash-template');
+var sass = require('gulp-sass');
+// var prettify = require('gulp-prettify');
 var path = require('path');
 var stringify = require('stringify-object');
 var notify = require('gulp-notify');
-var uglify = require('gulp-uglify');
+// var uglify = require('gulp-uglify');
 
-// var babel = require("gulp-babel");
+var babel = require("gulp-babel");
 
-// gulp.task("babel", function () {
-//   return gulp.src("js/babel-app/**")
-//     .pipe(babel())
-//     .pipe(gulp.dest("js/app/"));
-// });
+function babelChange(){
+    return gulp.src("js/babel-app/**")
+        .pipe(babel())
+        .pipe(gulp.dest("js/app/"));
+}
 
 // task without watch
-gulp.task('template-build', function () {
+function templateChange() {
     var templates = {};  
 
     gulp.src('assets/templates/*/*.html')
@@ -31,16 +35,34 @@ gulp.task('template-build', function () {
         // Append the above "file name" and "file path"
         fs.writeFile('templates.js', "var templates =  " + stringify(templates).replace(/[\n\r]*\s\s+/g, " "));
     })).on('error', function(err){ notify(err.message); });
-});
 
-// this runs the `bundle-templates` task before starting the watch (initial bundle)
-gulp.task('watch', ['template-build'], function () {
-    // now watch the templates and the js file and rebundle on change
-    gulp.watch(['assets/templates/*/*.html'], ['template-build']);
-    // If you want to watch sass files using gulp, uncomment the below code
-    // Alert: Better use Sass engine instead of this
-    // gulp.watch(['assets/styles/sass/*.scss', 'assets/styles/sass-utilities/*.scss'], ['sass']);
-});
+    // return gulp.src('assets/templates/**.html')
+    //     .pipe(template({
+    //       commonjs: true,
+    //       // amd: true, 
+    //       strict: true
+    //     }))
+    //     .pipe(gulp.dest('new/'));
+}
 
+function sassChange(){
+  gulp.src('assets/styles/sass/**.scss')
+    .pipe(sourcemaps.init())
+    // TO DO: remove comments while compiling sass to css "sourceComments: false" doesn't work.
+    .pipe(sass({outputStyle: 'expanded', sourceComments: false}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('assets/styles/css'));
+}
 
-gulp.task('default', ['template-build'], function () {});
+function watchChanges(){
+    templateChange();
+    sassChange();
+    babelChange();
+    gulp.watch(['assets/templates/**.html','assets/styles/sass/**/*.scss', "js/babel-app/**.js"], ['template', 'sass', 'babel']);
+}
+
+// Tasks
+gulp.task('sass', sassChange);
+gulp.task('template', templateChange);
+gulp.task('babel', babelChange);
+gulp.task('watch', watchChanges);
