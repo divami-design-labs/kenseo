746e071214b06b57217c05eb85fcdf45d61f0e86
NavyaSri <navyasri@divami.com>
2016-07-22 11:34:48 +0530

    Changes in Validation Plug-in

--- ui/js/app/components/validation.js
+++ ui/js/app/components/validation.js
@@ -6,36 +6,38 @@
 // for each error field display error message
 
 var validation = (function(){
+	var validatingCounter = 0;
 	var allValidations = {
-		'empty': function(value, $requiredFields){
-				this.fieldName = $requiredFields.parents('.field-section').find('.input-label').html() || $requiredFields.data('v-label');
+		'empty': function(value, $requiredField){
+				this.fieldName = $requiredField.parents('.field-section').find('.input-label').html() || $requiredField.data('v-label');
 				return !!$.trim(value).length;
 		},
-		'email': function(value, $requiredFields){
+		'email': function(value, $requiredField){
 				var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 				return filter.test(value);
 		},
 	}
 	// displays border color red if the input is invalid
-	var addErrorSkinning = function($requiredFields){
-		$requiredFields.parents('.field-section').find('.field-section-wrapper').addClass('error-field');
+	var addErrorSkinning = function($requiredField){
+		$requiredField.parents('.field-section').find('.field-section-wrapper').addClass('error-field');
 	}
 	// removes border color of input field if it is valid
-	var removeErrorSkinning = function($requiredFields){
-		$requiredFields.parents('.field-section').find('.field-section-wrapper').removeClass('error-field');
+	var removeErrorSkinning = function($requiredField){
+		$requiredField.parents('.field-section').find('.field-section-wrapper').removeClass('error-field');
 	}
 
-	var validateRequiredField = function($requiredFields, validatingTokens){
-		var value = $requiredFields.val();
+	var validateRequiredField = function($requiredField, validatingTokens){
+		var value = $requiredField.val();
 		for(var i = 0; i < validatingTokens.length; i++){
 			var validate = allValidations[validatingTokens[i]];
 			// validating required field based on validating token
-			if(!validate(value, $requiredFields)){
+			if(!validate(value, $requiredField)){
+				validatingCounter++;
 				// highlighting the error field
-				addErrorSkinning($requiredFields);
-				$requiredFields.parents(".field-section").find(".field-section-error-messages").children().hide();
+				addErrorSkinning($requiredField);
+				$requiredField.parents(".field-section").find(".field-section-error-messages").children().hide();
 				// displaying input field error message
-				$requiredFields.parents(".field-section").find("[trigger ='"+ validatingTokens[i]+"']").show();
+				$requiredField.parents(".field-section").find("[trigger ='"+ validatingTokens[i]+"']").show();
 				return true;
 			}
 		}
@@ -52,30 +54,40 @@ var validation = (function(){
 		var $form = $clickedBtn.parents('.k-form');
 		var $fieldSections = $form.find('.field-section');
 		validate($fieldSections);
+		if(validatingCounter == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	var validate = function($fieldSections) {
 		// finding the validating field
 		var $requiredFields = $fieldSections.find('.k-required');
-		// removes the error message of single input field
-		$fieldSections.each(function(){
+		//removes the error message of single input field
+		$requiredFieldSections = $fieldSections.filter(function(i, el){
+			return $(el).find('.k-required').length !== 0;
+		})
+		$requiredFieldSections.each(function(){
 			var $field = $(this).find('.field-section-wrapper');
 			removeErrorSkinning($field);
 			$field.parents(".field-section").find(".field-section-error-messages").children().hide();
 		});
 
-		_.each($requiredFields, function(){
+		_.each($requiredFields, function($requiredField){
 			// gets validating attributes of all required fields
 			var validatingString = $requiredFields.attr('data-validate-this');
 			if(validatingString){
 				var validatingTokens = validatingString.split(",");
 				// function to validate all the required fields
-				validateRequiredField($requiredFields, validatingTokens);
+				validateRequiredField($($requiredField), validatingTokens);
 			}
 		})
 	}
 	// function gets the clicked button element from dynamic-events.js file
 	var doFormValidate = function($clickedBtn){
+		//initializing counter
+		validatingCounter = 0;
 		if(isFormValidate($clickedBtn)){
 			return true;
 		}
